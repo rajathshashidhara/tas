17ef4da6326a0d514c0730175087537bbf4cbfff
Rajath Shashidhara <rajaths@cs.washington.edu>
2024-12-12 02:13:45 +0000

    [fastpath] Fix bug in random drops
    Signed-off-by: Rajath Shashidhara <rajaths@cs.washington.edu>

--- tas/fast/fastemu.c
+++ tas/fast/fastemu.c
@@ -288,7 +288,7 @@ static unsigned poll_rx(struct dataplane_context *ctx, uint32_t ts,
     /* Random drops enabled. */
     unsigned k;
     for (i = 0; i < n; i++) {
-      if (UNLIKELY(rte_rand() < config.fp_rand_drop_prob)) {
+      if (UNLIKELY(((uint32_t)rte_rand()) < config.fp_rand_drop_prob)) {
         bufcache_free(ctx, bhs[i]);
         bhs[i] = NULL;
       }
@@ -575,7 +575,7 @@ static inline void tx_flush(struct dataplane_context *ctx)
   if (config.fp_rand_drop) {
     unsigned k;
     for (i = 0; i < ctx->tx_num; i++) {
-      if (UNLIKELY(rte_rand() < config.fp_rand_drop_prob)) {
+      if (UNLIKELY(((uint32_t)rte_rand()) < config.fp_rand_drop_prob)) {
         bufcache_free(ctx, ctx->tx_handles[i]);
         ctx->tx_handles[i] = NULL;
       }
